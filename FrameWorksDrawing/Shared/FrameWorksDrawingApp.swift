306a80f8a718af28716fd21c3871535dac07d8a4
SergeyPolozov <sergey.polozov@sbermarket.ru>
2022-04-30 20:32:19 +0300

    initial

---
+++ FrameWorksDrawing/Shared/FrameWorksDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameWorksDrawingApp.swift
+//  Shared
+//
+//  Created by Сергей Полозов on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FrameWorksDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
